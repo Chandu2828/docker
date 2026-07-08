7b3a1c6a27ee1ffc65fb9cabd38cb7fd6346afb7
Chandu2828 <purnachand7569@gmail.com>
2026-07-08 12:57:07 +0530

    added files

--- main.tf
+++ main.tf
@@ -41,6 +41,18 @@ resource "aws_security_group" "allow_docker" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+        ingress {
+        from_port   = 8081
+        to_port     = 8081
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+        ingress {
+        from_port   = 8082
+        to_port     = 8082
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     egress {
         from_port   = 0
         to_port     = 0
