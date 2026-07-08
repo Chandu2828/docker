016e8fa5b32d939e90054788fed77ad30b08a14d
Chandu2828 <purnachand7569@gmail.com>
2026-07-08 12:54:11 +0530

    added files

--- main.tf
+++ main.tf
@@ -28,16 +28,23 @@ resource "aws_security_group" "allow_docker" {
     description = "Allow TLS inbound traffic and all outbound traffic"
 
     ingress {
-        from_port = 22
-        to_port   = 22
-        protocol  = "tcp"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
         cidr_blocks = ["${chomp(data.http.my_public_ip.response_body)}/32"]
     }
 
+        ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
-        from_port = 0
-        to_port   = 0
-        protocol = "-1"
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
